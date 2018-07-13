345a7ce6d71f3372fc55ecc6ac6ec83a4dce2de2
Matteo Zannini <matteo.zannini@outlook.com>
2018-07-13 19:10:36 +0200

    start LCD test

--- abos/abos.ino
+++ abos/abos.ino
@@ -1,9 +1,24 @@
+
+void setup(){}
+void loop() {}
+/*
+#include <LiquidCrystal.h>
+LiquidCrystal Display20x4(34, 35, 11, 12, 19, 20);
 void setup() {
   // put your setup code here, to run once:
-
+  Display20x4.begin(20, 4);
+  Display20x4.clear();
+  //Display20x4.cursor();
+  delay(1000);
+  //Display20x4.noCursor();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Display20x4.setCursor(8,1);
+  Display20x4.print("ABOX");
+  Display20x4.setCursor(0,3);
+  Display20x4.print("Powered by ABOS o.s.");
+  delay(500);
 }
+*/
