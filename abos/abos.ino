7b1ed0416378b889c5d070e18d5c52856d0b0664
Matteo Zannini <matteo.zannini@outlook.com>
2018-07-13 17:45:20 +0200

    tested X engine

--- abos/abos.ino
+++ abos/abos.ino
@@ -1,9 +1,63 @@
-void setup() {
-  // put your setup code here, to run once:
+/* UN GIRO DEL MOTORE SONO: 3200 step */
+/* Microsecondo 1 --> 0,001 Millisecondo */
 
+const int stepPin = 15;
+const int dirPin = 21;
+const int enable = 14;
+int passi;
+
+void setup() {
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  pinMode(enable, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("\n<Arduino is ready>\n");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(enable, HIGH);
+  passi = numeroStep();
+  Serial.println(passi);
+  moveEngine();
 }
+
+int numeroStep() {
+  String name = "";
+  char c;
+  if ((Serial.available() > 0)) {
+    Serial.print("Inserire il numero di step: ");
+    while (Serial.available() > 0) {
+      c = Serial.read();
+      if (c == '\n') {
+        //Serial.println(name);
+        name = "";
+      } else if (isDigit(c) || c == '-') {
+        name += c;
+        //Serial.println(c);
+      }
+    }
+    Serial.println(name);
+    return name.toInt();
+  } else {
+    delay(100);
+  }
+  return 0;
+}
+
+void moveEngine() {
+  if (passi >= 0) {
+    digitalWrite(dirPin, LOW);
+  } else {
+    digitalWrite(dirPin, HIGH);
+    passi = passi * -1;
+  }
+  digitalWrite(enable, LOW);
+  for (int x = 0; x < passi; x++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(300);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(300);
+  }
+}
+
+
