3ff8f2863302480bbff36714adabf7e111f3d1f7
Matteo Zannini <matteo.zannini@outlook.com>
2018-07-13 22:00:59 +0200

    implemented buttons

--- abos/abos.ino
+++ abos/abos.ino
@@ -4,18 +4,23 @@ void loop() {}
 */
 #include <LiquidCrystal.h>
 LiquidCrystal Display20x4(28, 29, 10, 11, 16, 17);
+const int menu = 17;
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(menu, INPUT);
   Display20x4.begin(20, 4);
   Display20x4.clear();
-  //Display20x4.cursor();
-  delay(1000);
-  //Display20x4.noCursor();
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  bootScreen();
+  int file = analogRead(menu);
+  Serial.println(file);
+}
+
+void bootScreen(){
   Display20x4.setCursor(8,1);
+  
   Display20x4.print("ABOX");
   Display20x4.setCursor(0,3);
   Display20x4.print("Powered by ABOS o.s.");
