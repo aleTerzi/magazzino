da5fb9bbd626670a4c75245866d9d6534ef50113
Matteo Zannini <matteo.zannini@outlook.com>
2018-08-02 17:23:47 +0200

    beginning of development

---
+++ abos/abos.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       abos.ino
+    Created:	02/08/2018 17:20:54
+    Author:     PC-MATTEOZ\Matteo
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
